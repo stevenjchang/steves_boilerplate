6e22b31c41893a2b45d09987f75f62e1e6ff396c
stevenjchang <stevenjchang@gmail.com>
2019-04-13 17:47:30 -0700

    add basic packages, devtool extension

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App';
 import Router from './router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>, 
+  document.getElementById('root')
+);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import instagram from './instagram';
+
+const rootReducer = combineReducers({
+  // instagram,
+});
+
+export default rootReducer;
