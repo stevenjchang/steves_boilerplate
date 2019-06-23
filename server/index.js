082b466ce06a3c66dc6a14e4ccc892b579fd0eaf
stevenjchang <stevenjchang@gmail.com>
2019-06-23 13:14:51 -0700

    add authRoute routes

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const db = require('./databaseInteractor/knex');
 const IP = process.env.IP || 'localhost';
 const PORT = process.env.PORT || '3000';
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
 
 app.get('/posts', db.getPosts);
 app.get('/db', db.selectTestTable);
